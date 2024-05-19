b9c4bfa402e04cf8ce2d59a5a8f88e9adccc6f5a
CatalinaC16 <catalina.ciochina16@gmail.com>
2024-05-19 23:55:26 +0300

    Small update

--- src/test/java/com/example/flowerShop/controller/ProductIntegrationTest.java
+++ src/test/java/com/example/flowerShop/controller/ProductIntegrationTest.java
@@ -1,18 +1,12 @@
 package com.example.flowerShop.controller;
 
-import com.example.flowerShop.dto.product.ProductDetailedDTO;
-import com.example.flowerShop.dto.review.ReviewDetailedDTO;
-import com.example.flowerShop.dto.user.UserGetDTO;
-import com.example.flowerShop.utils.user.UserRole;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
